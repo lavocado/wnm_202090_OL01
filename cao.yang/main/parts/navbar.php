b2481f58ef98504d40cc6b75127d62419ec25512
Yang Cao <yangcao@Yangs-Mac.local>
2020-10-25 14:12:21 -0700

    update

---
+++ cao.yang/main/parts/navbar.php
@@ -0,0 +1,15 @@
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+			</nav>
+		</div>
+</header>
