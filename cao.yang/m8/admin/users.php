7eed11f2ce89f0a4d4d25aedbe750fba1985ddd9
lavocado <cynthiacytsao@gmail.com>
2020-11-04 22:06:55 -0800

    save 1104

--- cao.yang/m8/admin/users.php
+++ cao.yang/m8/admin/users.php
@@ -1,17 +1,70 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 include "../lib/php/functions.php";
 
-$users = file_get_json("../data/users.json");
+$filename = "../data/users.json";
+$users = file_get_json("$filename");
+
+$empty_user = (object)[
+	"name"=>"",
+	"type"=>"",
+	"email"=>"",
+	"classes"=>[]
+
+];
+
+
+//print_p([$_GET,$_POST]);
+
+
+if(isset($_GET['action'])) {
+	switch ($_GET['action']) {
+		case "update":
+			$users[$_GET['id']]->name = $_POST['user-name'];
+			$users[$_GET['id']]->type = $_POST['user-type'];
+			$users[$_GET['id']]->email = $_POST['user-email'];
+			$users[$_GET['id']]->classes = explode(",", $_POST['user-classes']);
+
+
+			file_put_contents($filename, json_encode($users));
+			header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
+			break;
+		case "create":
+			$empty_user->name = $_POST['user-name'];
+			$empty_user->type = $_POST['user-type'];
+			$empty_user->email = $_POST['user-email'];
+			$empty_user->classes = explode(",", $_POST['user-classes']);
+
+			$id = count($users);
+
+			$users[] = $empty_user;
+
+			file_put_contents($filename, json_encode($users));
+			header("location:{$_SERVER['PHP_SELF']}?id=$id");
+			break;
+		case "delete":
+			break;
+
+
+	}
+}
+
 
 
 function showUserPage($user) {
 
+$id = $_GET['id'];
+$addoredit = $id == "new" ? "Add" : "Edit";
+$createorupdate = $id == "new" ? "create" : "update";
 $classes = implode(", ", $user->classes);
 
 
 
-//heardoc
+//heredoc
 echo <<<HTML
 <nav class="nav nav-crumbs">
 	<ul>
@@ -19,23 +72,28 @@ echo <<<HTML
 	</ul>	
 </nav>
 
-<div>
-	<form>
-	<h2>Edit Information</h2>
-		<div class="form-control">
-			<label class="form-label">Name</label>
-			<input type="text" value="$user->name" class="form-input">
-			<label class="form-label">Type</label>
-			<input type="text" value="$user->type" class="form-input">
-			<label class="form-label">Email</label>
-			<input type="text" value="$user->email" class="form-input">
-			<label class="form-label">Classes</label>
-			<input type="text" value="$classes" class="form-input">
+<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=$createorupdate">
+	<h2>$addoredit User</h2>
+	<div class="form-control">
+		<label class="form-label" for="user-name">Name</label>
+		<input type="text" placeholder="Enter the User Name" value="$user->name" class="form-input" name="user-name" id="user-name">
 	</div>
-			<div class="form-control">
-				<a href="users.php" class="form-button">Save</a>
-			</div>
+	<div class="form-control">
+		<label class="form-label" for="user-type">Type</label>
+		<input type="text" placeholder="Enter the User Type" value="$user->type" class="form-input" name="user-type" id="user-type">
 	</div>
+	<div class="form-control">
+		<label class="form-label" for="user-email">Email</label>
+		<input type="text" placeholder="Enter the User Email" value="$user->email" class="form-input" name="user-email" id="user-email">
+	</div>
+	<div class="form-control">
+		<label class="form-label" for="user-classes">Classes</label>
+		<input type="text" placeholder="Enter the User Classes, comma separated" value="$classes" class="form-input" name="user-classes" id="user-classes">
+	</div>
+	<div class="form-control">
+		<input type="submit" class="form-button" value="Save Changes">
+	</div>
+</form>
 HTML;
 }
 ?>
@@ -63,7 +121,8 @@ HTML;
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
 				<ul>
-					<li><a href="users.php">User List</a></li>
+					<li><a href="<?= $_SERVER['PHP_SELF'] ?>">User List</a></li>
+					<li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New User</a></li>
 				</ul>
 			</nav>
 		</div>
@@ -77,7 +136,7 @@ HTML;
 			<?php
 
 			if(isset($_GET['id'])) {
-				showUserPage($users[$_GET['id']]);
+				showUserPage($_GET['id'] == "new" ? $empty_user : $users[$_GET['id']]);
 			} else {
 
 
--- cao.yang/main/README.md
+++
@@ -1,6 +0,0 @@
-# Yang Cao
-
-## Relevant Links
--http://cao-yang.com
--http://cao-yang.com/AAU/WNM608//wnm_202090_OL01/cao.yang
--http://cao-yang.com/AAU/WNM608//wnm_202090_OL01//cao.yang/styleguide
